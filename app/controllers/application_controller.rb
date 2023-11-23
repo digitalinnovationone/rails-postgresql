5f72721d877e4773e9b6b45a563a86f5dd2e0ebe
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-11-23 17:35:57 -0300

    login com cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@ class ApplicationController < ActionController::Base
     before_action :valida_sessao
 
     def valida_sessao
-        # debugger
-        x = ""
+        token = cookies[:cookie_adm_logado]
+        if token.blank?
+            return redirect_to "/login"
+        end
+
+        begin
+            jwt_decode = JWT.decode(token, TOKEN_JWT, true, { algorithm: 'HS256' })
+            payload = jwt_decode[0]
+            hash_adm = JSON.parse(payload)
+            @adm = Administrador.find(hash_adm["id"])
+        rescue
+            flash[:error] = "Cookie inválido"
+            return redirect_to "/login"
+        end
     end
 end
---
+++ config/initializers/jwt.rb
@@ -0,0 +1,1 @@
+TOKEN_JWT = ENV["TOKEN_JWT"] || "233211231qwsdhdsqjwaa"
