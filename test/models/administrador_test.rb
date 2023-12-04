c78d11b2a84c3ee28a5f0e3c536f5c24859e7561
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-04 17:01:12 -0300

    iniciando com testes

--- test/models/administrador_test.rb
+++ test/models/administrador_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class AdministradorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # def setup
+  #   @administrador = Administrador.new(nome: "Luiz", email: "lu@gmail.com", senha: "12345")
   # end
+
+  test "deve ter o nome preenchido" do
+    debugger
+    # Arrange
+    @administrador = Administrador.new(nome: "Luiz", email: "lu@gmail.com", senha: "12345")
+
+    # Act
+    valido = @administrador.valid?
+
+    # Assert
+    assert valido
+  end
 end
