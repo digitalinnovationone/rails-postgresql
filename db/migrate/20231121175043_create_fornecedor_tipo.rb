928d76d490116dd8ecb1b45b0a1b9ca35ab346d1
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-11 09:50:20 -0300

    correção migration para produção

--- db/migrate/20231121175043_create_fornecedor_tipo.rb
+++ db/migrate/20231121175043_create_fornecedor_tipo.rb
@@ -1,6 +1,6 @@
 class CreateFornecedorTipo < ActiveRecord::Migration[7.1]
   def change
-    create_table :fornecedor_tipo do |t|
+    create_table :fornecedor_tipos do |t|
       t.string :nome, limit: 100
 
       t.timestamps
