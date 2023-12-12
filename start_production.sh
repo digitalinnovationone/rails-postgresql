1850dc12fd581dafbadbffbfa52420f32d32107a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-12 09:29:17 -0300

    ajustando script start em produção

--- start_production.sh
+++ start_production.sh
@@ -5,4 +5,4 @@ rvm use 3.2.1
 RAILS_ENV=production bundle install
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rails s > /tmp/rails_app.log 2>&1
+RAILS_ENV=production bundle exec rails s > /tmp/rails_app.log 2>&1 &
