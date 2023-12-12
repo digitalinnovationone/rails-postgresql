b89360158b6f01c73650a09f92a50ecb78dadfd6
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-12-12 09:25:53 -0300

    ajustando script start em produção

--- start_production.sh
+++ start_production.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 cd /root/rails-postgresql
-source /usr/local/rvm/scripts/rvm 
+source /usr/local/rvm/scripts/rvm
 rvm use 3.2.1
 RAILS_ENV=production bundle install
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rails s > /tmp/rails_app.log 2>&1 &
+RAILS_ENV=production bundle exec rails s > /tmp/rails_app.log 2>&1
